162d50433cd6f7aacd5fc968c21b13a0d4528acc
Zaharov89 <70170730+Zaharov89@users.noreply.github.com>
2023-05-26 14:04:16 +0300

    Дз 3 задание 3 попытка 3

--- SkyPRO/Lesson_3/lesson_3_task_3.py
+++ SkyPRO/Lesson_3/lesson_3_task_3.py
@@ -1,11 +1,12 @@
-from SkyPRO.Lesson_3 import mailing
+from SkyPRO.Lesson_3.mailing import Mailing
+from SkyPRO.Lesson_3.address import Address
 
 
-my_mailing = (
-"Отправление", mailing.say_track(), "из", mailing.say_from_address2(), "в", mailing.say_to_address2(), "Стоимость",
-mailing.say_cost(), "рублей.")
+#my_mailing = (
+#"Отправление", mailing.say_track(), "из", mailing.say_from_address2(), "в", mailing.say_to_address2(), "Стоимость",
+#mailing.say_cost(), "рублей.")
 
 
-
+my_mailing = Mailing("325745", "Магадан", "Перова", "22", "55", "351345", "Москва", "Ленина", "34", "11", 123, "123")
 
 print(my_mailing)
--- SkyPRO/Lesson_3/mailing.py
+++ SkyPRO/Lesson_3/mailing.py
@@ -1,23 +1,29 @@
-from SkyPRO.Lesson_3 import address
+from SkyPRO.Lesson_3.address import Address
 
 
 class Mailing:
 
-    def __init__(self, address.from_address(), address.to_address(), cost, track):
-        self.from_address = address.from_address.say_from_address()
-        self.to_address = address.to_address.say_to_address()
+    def __init__(self, cost, track):
         self.cost = int(cost)
         self.track = str(track)
 
-    def say_from_address(self):
-        print(self.from_address)
+    def add_to(self):
+        to_address = Address("351345", "Москва", "Ленина", "34", "11")            # Создаём ОБЪЕКТ КЛАССА
+        to_address.say_to_address()                                               # Вызываем метод ИЗ ОБЪЕКТА
 
-    def say_to_address(self):
-        print(self.to_address)
+    def add_from(self):
+        from_address = Address("325745", "Магадан", "Перова", "22", "55")         # Создаём ОБЪЕКТ КЛАССА
+        from_address.say_from_address()                                           # Вызываем метод ИЗ ОБЪЕКТА
 
-    def say_cost(self):
-        print(self.cost)
 
-    def say_track(self):
-        print(self.track)
+    # def say_from_address(self):
+    #     print(self.from_address)
+    #
+    # def say_to_address(self):
+    #     print(self.to_address)
 
+    # def say_cost(self):
+    #     print(self.cost)
+    #
+    # def say_track(self):
+    #     print(self.track)
