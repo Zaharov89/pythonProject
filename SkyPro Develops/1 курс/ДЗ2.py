b67dd57550fc9161dbe49c473832ce6918262803
Zaharov89 <70170730+Zaharov89@users.noreply.github.com>
2023-07-06 23:33:37 +0300

    Вношу изменения 2

--- SkyPro Develops/1 курс/ДЗ2.py
+++ SkyPro Develops/1 курс/ДЗ2.py
@@ -1,13 +1,11 @@
-print("Привет! Предлагаю проверить свои знания английского! Наберите \'ready\', чтобы начать!")
-
-user_input = input()
+user_input = input("Привет! Предлагаю проверить свои знания английского! Наберите \'ready\', чтобы начать! ")
 
 # Создаем списки с вопросами и ответами
 
 questions = ["My name ___ Vova", "I ___ a coder", "I live ___ Moscow"]
 answers = ["is", "am", "in"]
 
-all_answer = 0
+all_correct_answer = 0
 
 # Начинаем решать задачу
 
@@ -19,10 +17,11 @@ else:                                                      # Начинаем з
         user_answer = input()
         if user_answer == answers[i]:
             print('Ответ верный!')
-            all_answer += 1
+            all_correct_answer += 1
         else:
             print(f'Неправильно. Правильный ответ: {answers[i]}')
-print(f'Вот и все! Вы ответили на {all_answer} вопросов из {len(answers)} верно, это {round(100 * all_answer / len(answers))} процентов.')
+
+        print(f'Вот и все! Вы ответили на {all_correct_answer} вопросов из {len(answers)} верно, это {round(100 * all_correct_answer / len(answers))} процентов.')
 
 
 
