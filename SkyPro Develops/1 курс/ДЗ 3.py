f099011578d5a0658c676a703e3a917bf61a52f5
Zaharov89 <70170730+Zaharov89@users.noreply.github.com>
2023-07-14 21:07:11 +0300

    Вношу изменения 2

--- SkyPro Develops/1 курс/ДЗ 3.py
+++ SkyPro Develops/1 курс/ДЗ 3.py
@@ -35,19 +35,19 @@ answers = {}
 
 words_level = {'Легкий': words_easy,'Средний': words_medium,'Тяжелый': words_hard}    # Создаем словарь с разными уровнями
 
-words = input('Введите уровень сложности словаря: ')                                  # Пользователь вводит желаемый уровень
+words = input('Введите уровень сложности словаря c большой буквы: ').title()          # Пользователь вводит желаемый уровень
 
-point = words_level[words]                                                            # Копируем словарь с выбранным уровнем сложности от пользователя
-
-for key, values in point.items():                                                     # Создаем цикл выбранного словаря с ключом и значением
-    # Программа: divide, 9 букв, начинается на р...
-    user_answer = input(f'{key}, {len(values)} букв, начинается на {values[0]}: ')
-    if user_answer == values:
-        answers[key] = True
-        print(f'Верно, {key} — это {values}.')
-    else:
-        answers[key] = False
-        print(f'Неверно. {key} — это {values}.')
+if words in words_level.keys():
+    point = words_level[words]                                                        # Копируем словарь с выбранным уровнем сложности от пользователя
+    for key, values in point.items():                                                 # Создаем цикл выбранного словаря с ключом и значением
+        # Программа: divide, 9 букв, начинается на р...
+        user_answer = input(f'{key}, {len(values)} букв, начинается на {values[0]}: ')
+        if user_answer == values:
+            answers[key] = True
+            print(f'Верно, {key} — это {values}.')
+        else:
+            answers[key] = False
+            print(f'Неверно. {key} — это {values}.')
 
 # Когда слова закончились, выведите в зависимости от результата:
 # Правильно отвечены слова:
@@ -59,21 +59,24 @@ for key, values in point.items():                                               
 # Неправильно отвечены слова:
 # hero
 
-print('Правильно отвечены слова: ')
+    print('Правильно отвечены слова: ')
 
-numbers = 0
+    numbers = 0
 
-for word, word_values in answers.items():
-    if word_values:
-        numbers += 1
-        print(word)
+    for word, word_values in answers.items():
+        if word_values:
+            numbers += 1
+            print(word)
 
-print('Неправильно отвечены слова: ')
+    print('Неправильно отвечены слова: ')
 
-for word, word_values in answers.items():
-    if not word_values:
-        print(word)
+    for word, word_values in answers.items():
+        if not word_values:
+            print(word)
 
 # Посчитайте количество правильно отгаданных слов, например, получив список ключей из answers. Используйте его в качестве ключа, чтобы получить ранг пользователя.
 
-print(f'Ваш ранг: ', levels[numbers], sep='\n')
+    print(f'Ваш ранг: ', levels[numbers], sep='\n')
+
+else:
+    print("Я не знаю такого уровня")
